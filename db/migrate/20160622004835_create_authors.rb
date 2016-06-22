c64341238733da3464f996887364883e5317760e
Brian Kung <brian@callmekung.com>
2016-06-21 20:07:41 -0500

    Forgot to give authors a uuid

--- db/migrate/20160622004835_create_authors.rb
+++ db/migrate/20160622004835_create_authors.rb
@@ -1,6 +1,6 @@
 class CreateAuthors < ActiveRecord::Migration[5.0]
   def change
-    create_table :authors do |t|
+    create_table :authors, id: :uuid do |t|
       t.string :name
       t.string :title
 
