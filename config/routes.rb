589adb5b92558b469533bf2f8545861a729d5bae
Brian Kung <brian@callmekung.com>
2016-07-06 17:11:10 -0500

    Adds routes for adding a homepage list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homepage#show'
+  root 'homepage#show'
   resources :papers
   resources :lists
+
+  post 'homepages_lists/:list_id', to: 'homepages_lists#create', as: 'homepages_list'
+  delete 'homepages_lists/:list_id', to: 'homepages_lists#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
