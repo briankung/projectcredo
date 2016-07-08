553ce1e47224c138b2d3d814653a98dbbda7ce1a
Brian Kung <brian@callmekung.com>
2016-07-08 17:54:24 -0500

    Changes the route / controller to pins

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'homepage#show'
+
+  devise_for :users
+
   resources :papers
   resources :lists
 
-  post 'homepages_lists/:list_id', to: 'homepages_lists#create', as: 'homepages_list'
-  delete 'homepages_lists/:list_id', to: 'homepages_lists#destroy'
+  post 'pins/:list_id', to: 'pins#create', as: 'pins'
+  delete 'pins/:list_id', to: 'pins#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
