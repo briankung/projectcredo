c3832354d2d6cf6369a787c9dca4b672659bfcae
wsf246 <willshaofan@gmail.com>
2016-07-11 19:04:03 -0500

    convert pubmed uids to paper metadata

--- app/controllers/concerns/pubmedsearch.rb
+++ app/controllers/concerns/pubmedsearch.rb
@@ -4,10 +4,14 @@ module PubmedSearch
   def search_pubmed(search_term)
     @search_terms = search_term.split.join("+")
     search_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=20&term='+@search_terms
-    puts search_url
-    uri = URI.parse(search_url)
+    search_uri = URI.parse(search_url)
+    search_response = Net::HTTP.get(search_uri)
+    uids = JSON.parse(search_response)['esearchresult']['idlist'].map {|uid| uid}.join(",")
+
+
+    metadata_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&rettype=abstract&id='+uids
+    uri = URI.parse(metadata_url)
     response = Net::HTTP.get(uri)
-    @search_results = JSON.parse(response) 
-    
+    @results = JSON.parse(response)    
   end
 end
