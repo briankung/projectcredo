fc6c595554b8f3d239da20b76f6c2f308b33a456
Brian Kung <brian@callmekung.com>
2016-08-08 17:00:22 -0500

    Cosmetic change

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ApplicationRecord
   acts_as_taggable
   acts_as_votable
-  default_scope {order('updated_at DESC')}
+  default_scope { order updated_at: :desc }
 
   has_and_belongs_to_many :homepages
   belongs_to :user
