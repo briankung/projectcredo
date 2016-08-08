1bc16b5b837dc4144664afaada1b829e4bdbed31
William Fan <William Fan>
2016-08-08 18:25:06 -0500

    adds validations for tag

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -8,4 +8,11 @@ class Paper < ApplicationRecord
 
   accepts_nested_attributes_for :authors, reject_if: proc { |attributes| attributes['name'].blank? }
   validates_associated :authors
+  validate :bias_list_inclusion
+
+  def bias_list_inclusion
+    bias_list.each do |bias|
+      errors.add(bias,"is not valid") unless %w(selection performance detection attrition reporting small\ sample\ size).include?(bias)
+    end
+  end
 end
