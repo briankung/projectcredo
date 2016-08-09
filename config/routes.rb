c170c47046ae25d44327f0390bfa2838f1e2eb89
Brian Kung <brian@callmekung.com>
2016-08-09 18:47:49 -0500

    Adds reference voting routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
       resource :vote, only: [:create, :destroy], as: :list_vote
     end
   end
+
+  post 'references/:id/vote' => 'votes#create', as: :reference_vote
+  delete 'references/:id/vote' => 'votes#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
