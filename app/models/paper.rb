9f3ea31b67b926707627fb5a7ea1a2d1bf5b1d07
William Fan <William Fan>
2016-08-09 18:30:11 -0500

    Add methodologies validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -9,11 +9,17 @@ class Paper < ApplicationRecord
 
   accepts_nested_attributes_for :authors, reject_if: proc { |attributes| attributes['name'].blank? }
   validates_associated :authors
-  validate :bias_list_inclusion
+  validate :bias_list_inclusion, :methodology_list_inclusion
 
   def bias_list_inclusion
     bias_list.each do |bias|
       errors.add(bias,"is not valid") unless %w(selection performance detection attrition reporting small\ sample\ size).include?(bias)
+    end
+  end
+
+  def methodology_list_inclusion
+    methodology_list.each do |methodology|
+      errors.add(methodology,"is not valid") unless %w(meta-analysis systematic\ review randomized\ control\ trial quasi-experiment cohort case-control cross-sectional\ survey case\ report).include?(methodology)
     end
   end
 end
