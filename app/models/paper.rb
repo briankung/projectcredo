03c0b76a9981852f00e73da149c10be713557c67
William Fan <William Fan>
2016-08-09 18:19:42 -0500

    add methodologies tags

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,6 @@
 class Paper < ApplicationRecord
   acts_as_taggable
-  acts_as_taggable_on :biases
+  acts_as_taggable_on :biases, :methodologies
   ActsAsTaggableOn.strict_case_match = true
 
   has_and_belongs_to_many :authors
