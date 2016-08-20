fb9b58119e44e76cc96b86040c9392c7f4d7e434
William Fan <William Fan>
2016-08-20 16:34:28 -0500

    setup relationship between publisher and paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -5,6 +5,7 @@ class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
   has_many :lists, through: :references
   has_many :references, dependent: :destroy
+  belongs_to :publisher
 
   accepts_nested_attributes_for :authors, reject_if: proc { |attributes| attributes['name'].blank? }
   validates_associated :authors
