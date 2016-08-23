9db028ce9a30caa94ede8b5f53787269143af8de
William Fan <William Fan>
2016-08-23 14:04:58 -0500

    rename publisher migrations to publication

--- db/migrate/20160820205817_create_publications.rb
+++ db/migrate/20160820205817_create_publications.rb
@@ -1,4 +1,4 @@
-class CreatePublishers < ActiveRecord::Migration[5.0]
+class CreatePublications < ActiveRecord::Migration[5.0]
   def change
     create_table :publications, id: :uuid  do |t|
       t.string :name
--- db/migrate/20160820211525_add_publication_id_to_papers.rb
+++ db/migrate/20160820211525_add_publication_id_to_papers.rb
@@ -1,4 +1,4 @@
-class AddPublisherIdToPapers < ActiveRecord::Migration[5.0]
+class AddPublicationIdToPapers < ActiveRecord::Migration[5.0]
   def change
     add_column :papers, :publication_id, :uuid
   end
