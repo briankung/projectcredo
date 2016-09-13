bd9ccc1d5188018f4e9ce2f8322e79ce47719f79
William Fan <willshaofan@gmail.com>
2016-09-12 19:51:08 -0500

    update views for login/singup

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,11 +24,10 @@ class User < ApplicationRecord
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    conditions[:email].downcase! if conditions[:email]
-    where(conditions.to_h).first
     if login = conditions.delete(:login)
       where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
       where(conditions.to_h).first
     end
   end
