7355e84ab03fc59213ac41b257f80e01e5117554
Brian Kung <brian@callmekung.com>
2016-09-14 15:55:39 -0500

    Adds .row to whitelisted clickable nodes

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -6,9 +6,8 @@ $(document).ready(function() {
   $('div.panel-body').on('click', function(e) {
     target = $(e.target)
 
-    // I'm whitelisting .col-md-12 as a child node of div.panel-body that can act as a clickable link.
-    // This is very brittle.
-    if (target.hasClass('col-md-12')) {
+    // I'm whitelisting .col-md-12 and rows as nodes that can act as a clickable link. This is very brittle.
+    if (target.hasClass('col-md-12') || target.hasClass('row')) {
       panel = target.closest('.panel-body');
       // I'm mixing jQuery and Javascript with the [0].click().
       // This is so that I can force clicking through to the URL of an <a> tag, which jQuery doesn't do.
