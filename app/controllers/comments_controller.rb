ab5158dfaa040bce32fe3bfb2ab432a103fd0337
Brian Kung <brian@callmekung.com>
2016-09-14 23:25:18 -0500

    Adds comments to references

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,58 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
+  # GET /lists/1/edit
+  def edit
+  end
+
+  # POST /comments
+  # POST /comments.json
+  def create
+    @comment = Comment.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to :back, notice: 'Comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @comment }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /comments/1
+  # DELETE /comments/1.json
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to :back, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:content, :parent_id, :parent_type, :user_id)
+    end
+end
---
+++ app/helpers/comments_helper.rb
@@ -0,0 +1,9 @@
+module CommentsHelper
+  def comments_tree_for(comments)
+    comments.map do |comment, nested_comments|
+      render(comment) do
+        content_tag(:div, comments_tree_for(nested_comments), class: 'reply well') if nested_comments.size > 0
+      end
+    end.join.html_safe
+  end
+end
