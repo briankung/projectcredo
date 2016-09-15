44cb7a48b7bf7a01cdae80039c63b66b54e607b5
Brian Kung <brian@callmekung.com>
2016-09-14 20:16:56 -0500

    Refactors reference votes routes, adds comment reference comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,12 @@ Rails.application.routes.draw do
     end
   end
 
-  post 'references/:id/vote' => 'votes#create', as: :reference_vote
-  delete 'references/:id/vote' => 'votes#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :reference do
+    scope ":id" do
+      post 'vote' => 'votes#create', as: :vote
+      delete 'vote' => 'votes#destroy'
+
+      resources :comments, only: [:edit, :create, :update, :destroy]
+    end
+  end
 end
