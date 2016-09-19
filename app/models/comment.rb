819a166b2a2c544bc869352e4ab7c23540cc8dd7
William Fan <willshaofan@gmail.com>
2016-09-19 18:08:51 -0500

    comments sorted by votes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :parent, polymorphic: true
 
-  has_closure_tree
+  has_closure_tree order: 'cached_votes_up DESC'
   acts_as_votable
 
   validates :content, presence: true
