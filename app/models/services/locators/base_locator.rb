b288ede7441f440b2048748ddd7db984e0933e4b
Brian Kung <brian@callmekung.com>
2016-09-26 17:22:43 -0500

    Strips locator id of leading and trailing whitespace

--- app/models/services/locators/base_locator.rb
+++ app/models/services/locators/base_locator.rb
@@ -2,7 +2,7 @@ class BaseLocator
   attr_accessor :column, :locator_id
 
   def initialize locator_id
-    self.locator_id = locator_id
+    self.locator_id = locator_id.strip
   end
 
   def find_paper
