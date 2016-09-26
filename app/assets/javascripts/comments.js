cec5af25dff328a5efb0b197356a3e777e328cd0
William Fan <willshaofan@gmail.com>
2016-09-25 22:52:26 -0500

    fix javascript for reply

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.toggle-reply').on('click', function(e) {
     e.preventDefault();
-    $(e.target).next('.comment-reply').toggleClass('hidden');
+    $(e.target).parents("ul").next('.comment-reply').toggleClass('hidden');
   });
 });
