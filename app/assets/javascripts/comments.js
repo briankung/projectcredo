ba627d6996b27ca4a6da346dff28d4e08b53903d
William Fan <willshaofan@gmail.com>
2016-09-25 22:04:47 -0500

    remove unnecessary js

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -3,14 +3,4 @@ $(document).ready(function() {
     e.preventDefault();
     $(e.target).next('.comment-reply').toggleClass('hidden');
   });
-
-  if($('#text_area').val() ==  "")
-     $('#submitButtonId').attr('disabled', true);
-
-  $('#text_field').keyup(function(){
-        if($('#text_field').val() !=  "")
-             $('#submitButtonId').attr('disabled', false);
-    else
-       $('#submitButtonId').attr('disabled', true);
-  });
 });
