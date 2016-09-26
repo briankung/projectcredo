c9197881da8a38fb136030fea98d850991811c1f
Brian Kung <brian@callmekung.com>
2016-09-26 17:34:36 -0500

    Allows creation of stub Papers with just links

--- app/models/services/locators/link_paper_locator.rb
+++ app/models/services/locators/link_paper_locator.rb
@@ -1,5 +1,20 @@
 class LinkPaperLocator < BaseLocator
+  attr_accessor :paper_title
+
+  def initialize locator_id, paper_title
+    self.locator_id = locator_id.strip
+    self.paper_title = paper_title.strip
+  end
+
   def column
     'link'
+  end
+
+  def find_paper
+    if (paper = super)
+      return paper
+    end
+
+    return Paper.create title: paper_title, link: self.locator_id
   end
 end
