e5d2ac310d29641bb0db8204bbe381ef239cfada
William Fan <willshaofan@gmail.com>
2016-09-28 12:43:52 -0500

    change render :back to redirect_to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,7 +31,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to :back, notice: 'Comment was successfully updated.' }
         format.json { render :show, status: :ok, location: @comment }
       else
-        format.html { render :back }
+        format.html { redirect_to :back, notice: 'Comment was not updated.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
