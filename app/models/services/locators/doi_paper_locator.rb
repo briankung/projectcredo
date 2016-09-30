44f11354f191c130d7e334d7f09e92251645e4f3
William Fan <willshaofan@gmail.com>
2016-09-30 18:14:21 -0500

    Have DOI's searched through pubmed

--- app/models/services/locators/doi_paper_locator.rb
+++ app/models/services/locators/doi_paper_locator.rb
@@ -2,4 +2,31 @@ class DoiPaperLocator < BaseLocator
   def column
     'doi'
   end
+
+  def find_paper
+    if (paper = super)
+      return paper
+    end
+
+    pubmed = Pubmed.new
+    uid = pubmed.get_search_result_ids(self.locator_id)
+    results = pubmed.search(uid)
+    data = results['result'][uid]
+    names = data['authors'].map {|a| a['name']}
+
+    existing_authors = Author.where(name: names)
+    existing_names = existing_authors.map(&:name)
+    new_authors = (names - existing_names).map {|a| Author.create name: a}
+
+    paper = Paper.create(
+      pubmed_id: data['uid'],
+      title: data['title'],
+      published_at: data['pubdate'],
+      authors: (existing_authors + new_authors),
+      abstract: pubmed.get_abstract(data['uid']),
+      doi: data['elocationid'].sub(/^doi: /, ""),
+      publication: data['source']
+    )
+    return paper
+  end
 end
--- app/models/services/pubmed.rb
+++ app/models/services/pubmed.rb
@@ -57,17 +57,17 @@ class Pubmed
     return paper
   end
 
+  def get_search_result_ids query
+    query = query.gsub(' ', '+')
+    search_uri = generate_uri @search_url, @default_parameters.merge(term: query)
+    search_response = JSON.parse Net::HTTP.get(search_uri)
+
+    # Someday: It would be nice to validate that these search results are in the format we expect
+    search_response['esearchresult']['idlist'].join(",")
+  end
+
   private
     def generate_uri(url, parameters)
       URI.parse(url + '?' + URI.encode_www_form(parameters))
-    end
-
-    def get_search_result_ids query
-      query = query.gsub(' ', '+')
-      search_uri = generate_uri @search_url, @default_parameters.merge(term: query)
-      search_response = JSON.parse Net::HTTP.get(search_uri)
-
-      # Someday: It would be nice to validate that these search results are in the format we expect
-      search_response['esearchresult']['idlist'].join(",")
     end
 end
