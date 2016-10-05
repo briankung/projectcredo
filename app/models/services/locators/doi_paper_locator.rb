d739e0da0bbe2d6f97fe48478cb56db084fa5fac
William Fan <willshaofan@gmail.com>
2016-10-04 23:49:48 -0500

    refactor DOIPaperLocator findpaper

--- app/models/services/locators/doi_paper_locator.rb
+++ app/models/services/locators/doi_paper_locator.rb
@@ -6,74 +6,26 @@ class DoiPaperLocator < BaseLocator
   def find_paper
     return super if super
 
+    paper = Paper.new
+
+    crossref = Crossref.new
+    if result = crossref.get_doi_metadata(self.locator_id)
+      data = result['message']
+      paper = self.import_data_to_paper(paper,data,'crossref')
+    end
+
     pubmed = Pubmed.new
     if uid = pubmed.find_uid_by_doi(self.locator_id)
       result = pubmed.get_uid_metadata(uid)
       data = result['result'][uid]
-      names = data['authors'].map {|a| a['name']}
+      paper = self.import_data_to_paper(paper,data,'pubmed')
+    end
 
-      existing_authors = Author.where(name: names)
-      existing_names = existing_authors.map(&:name)
-      new_authors = (names - existing_names).map {|a| Author.create name: a}
-
-      paper = Paper.create(
-        pubmed_id: data['uid'],
-        title: data['title'],
-        published_at: data['pubdate'],
-        authors: (existing_authors + new_authors),
-        abstract: pubmed.get_abstract(data['uid']),
-        doi: self.locator_id,
-        publication: data['source']
-      )
+    if paper.save
       return paper
     else
-      paper = Paper.new
-      paper.errors.add(:locator_id, "is invalid; no paper found for searched DOI: #{self.locator_id}")
+      paper.errors.add(:locator_id, "is invalid; no paper found for searched DOI: #{self.locator_id}") if paper.errors.empty?
       return paper
     end
-  end
-
-  def import_data_to_paper(paper,imported_data,source)
-    source = source.downcase
-
-    if source == "pubmed"
-      if paper.authors.empty?
-        names = imported_data['authors'].map {|a| a['name']}
-        existing_authors = Author.where(name: names)
-        existing_names = existing_authors.map(&:name)
-        new_authors = (names - existing_names).map {|a| Author.create name: a}
-      end
-
-      doi = imported_data['elocationid']
-      if doi.blank?
-        doi = imported_data['articleids'].find {|id| id['idtype'] == 'doi' }['value']
-      end
-
-      paper.pubmed_id ||= imported_data['uid']
-      paper.title ||= imported_data['title']
-      paper.published_at ||=  imported_data['pubdate']
-      paper.authors ||= (existing_authors + new_authors)
-      paper.abstract ||= Pubmed.new.get_abstract(imported_data['uid'])
-      paper.doi ||= doi
-      paper.publication ||= imported_data['source']
-    end
-
-    if source == "crossref"
-      if paper.authors.empty?
-        names = imported_data['author'].map {|a| "#{a['given']} #{a['family']}"}
-        existing_authors = Author.where(name: names)
-        existing_names = existing_authors.map(&:name)
-        new_authors = (names - existing_names).map {|a| Author.create name: a}
-      end
-
-      paper.title ||= imported_data['title'].first
-      paper.published_at ||=  imported_data['created']['date-time'].to_date
-      paper.authors = (existing_authors + new_authors) if paper.authors.empty?
-      paper.doi ||= imported_data['DOI']
-      paper.publication ||= imported_data['publisher']
-      paper.link ||= imported_data['link'].first['URL'] if imported_data['link']
-    end
-
-    return paper
   end
 end
