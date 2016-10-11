33ef637d794aa8b3998ec925f36bce54a1f4f793
Brian Kung <brian@callmekung.com>
2016-10-11 16:01:01 -0500

    Sets slug before creating a list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,26 @@
 class List < ApplicationRecord
   acts_as_taggable
   acts_as_votable
+
   default_scope { order( cached_votes_up: :desc, updated_at: :desc ) }
+
+  before_create :set_slug
 
   has_and_belongs_to_many :homepages
   belongs_to :user
 
   has_many :papers, through: :references
   has_many :references, dependent: :destroy
+
+  def to_slug
+    self.name.downcase.gsub(/[^\p{N}\p{L}]/, '-').gsub(/-{2,}/, '-')
+  end
+
+  def set_slug
+    self.slug = self.to_slug
+  end
+
+  def set_slug!
+    self.update_column :slug, self.to_slug;
+  end
 end
