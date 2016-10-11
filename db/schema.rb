25b1075b9bc52eb52fda669e1f52c5ce4e2a4bce
Brian Kung <brian@callmekung.com>
2016-10-11 15:56:58 -0500

    Adds slug column to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011002653) do
+ActiveRecord::Schema.define(version: 20161011200024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20161011002653) do
     t.string   "description"
     t.integer  "cached_votes_up", default: 0
     t.integer  "user_id",                     null: false
+    t.string   "slug"
     t.index ["cached_votes_up", "created_at"], name: "index_lists_on_cached_votes_up_and_created_at", order: {"cached_votes_up"=>:desc, "created_at"=>:desc}, using: :btree
     t.index ["user_id"], name: "index_lists_on_user_id", using: :btree
   end
