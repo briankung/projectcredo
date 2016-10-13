c98bb150f3c95961495dcf1678c518524e75d193
Brian Kung <brian@callmekung.com>
2016-10-13 18:54:05 -0500

    Updates render calls to explicitly use particular class views ...which makes the views more portable.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   def comments_tree_for(comments)
     comments.map do |comment, nested_comments|
-      render(comment) do
+      render 'comments/comment', comment: comment do
         content_tag(:div, comments_tree_for(nested_comments), class: 'reply') if nested_comments.size > 0
       end
     end.join.html_safe
