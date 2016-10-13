a1077c7f2df9df49dbbfabefa55848cf28774230
Brian Kung <brian@callmekung.com>
2016-10-13 18:48:32 -0500

    Explicitly render other controller views

--- app/controllers/users/lists_controller.rb
+++ app/controllers/users/lists_controller.rb
@@ -1,17 +1,13 @@
 class Users::ListsController < ApplicationController
-  before_action :set_user
-
   def index
+    @user = User.find_by username: params[:id]
     @lists = @user.lists
+    render 'lists/index'
   end
 
   def show
-    @list = @user.lists.find_by slug: params[:list_slug]
+    @list = List.find_by slug: params[:id]
     @references = @list.references
+    render 'lists/show'
   end
-
-  private
-    def set_user
-      @user = User.find_by username: params[:username]
-    end
 end
--- app/controllers/users/references_controller.rb
+++ app/controllers/users/references_controller.rb
@@ -1,13 +1,6 @@
 class Users::ReferencesController < ApplicationController
-  before_action :set_user
-
   def show
-    @list = @user.lists.find_by slug: params[:list_slug]
-    @reference = @list.references.find_by id: params[:reference_id]
+    @reference = Reference.find_by id: params[:id]
+    render 'references/show'
   end
-
-  private
-    def set_user
-      @user = User.find_by username: params[:username]
-    end
 end
