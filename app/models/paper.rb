4715bcc010feecf8879e220310bdddd5e87369c3
William Fan <willshaofan@gmail.com>
2016-10-13 14:53:54 -0500

    associate links to papers

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -6,6 +6,7 @@ class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
   has_many :lists, through: :references
   has_many :references, dependent: :destroy
+  has_many :links
 
   accepts_nested_attributes_for :authors, reject_if: proc { |attributes| attributes['name'].blank? }
   validates_associated :authors
