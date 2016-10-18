cc4b37a40e331f6ec56759fa477a5ed96fd66920
Brian Kung <brian@callmekung.com>
2016-10-18 15:17:22 -0500

    Missed one in a refactoring

--- app/controllers/users/references_controller.rb
+++ app/controllers/users/references_controller.rb
@@ -3,7 +3,7 @@ class Users::ReferencesController < ApplicationController
     user = User.find_by username: params[:username]
     list = user.lists.find_by slug: params[:list_slug]
 
-    @reference = @list.references.find_by id: params[:reference_id]
+    @reference = list.references.find_by id: params[:reference_id]
     render 'references/show'
   end
 end
