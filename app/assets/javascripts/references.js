349d97cbe4ef2f511c890c8fa788f5408a922117
Brian Kung <brian@callmekung.com>
2016-10-21 15:08:39 -0500

    Simplifies js and adds doi

--- app/assets/javascripts/references.js
+++ app/assets/javascripts/references.js
@@ -11,20 +11,14 @@ $(document).ready(function() {
       hideResults();
     } else {
       $.get('https://search.crossref.org/dois?sort=score&q=' + this.value).done(function(data) {
-        var results, resultsByFullCitation;
-        resultsByFullCitation = {}
+        var results;
 
         if (data.length > 0) {
-          var citations;
-
-          data.forEach(function(datum) {
-            var caseSensitiveCitation;
-            caseSensitiveCitation = datum.fullCitation || datum.title;
-            resultsByFullCitation[datum.fullCitation.toLowerCase()] = datum.fullCitation || datum.title;
-          });
-
           resultsElement.children().remove();
-          results = $.map(resultsByFullCitation, function(citation) {return "<li class='list-group-item'>" + citation + "</li>"});
+
+          results = data.map(function(result) {
+            return '<li class="list-group-item" data-doi="' + result.doi.replace('http://dx.doi.org/', '') + '">' + result.fullCitation + "</li>"
+          });
           resultsElement.append(results.join(''));
           resultsElement.toggleClass('hidden', false);
         } else {
