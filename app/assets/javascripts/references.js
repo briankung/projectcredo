9bc52cd6409ecae13c3c3342b5d8dc9713ed7c83
Brian Kung <brian@callmekung.com>
2016-10-21 00:36:10 -0500

    Adds crossref search functions

--- app/assets/javascripts/references.js
+++ app/assets/javascripts/references.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
+  var showCrossrefResults = debounce(function() {
+    var resultsElement = $('#crossref-results');
+
+    if (this.value === '') {
+      resultsElement.toggleClass('hidden', true);
+      resultsElement.children().remove();
+    } else {
+      $.get('https://search.crossref.org/dois?sort=score&q=' + this.value).done(function(data) {
+        var results, resultsByFullCitation;
+        resultsByFullCitation = {}
+
+        if (data.length > 0) {
+          var citations;
+
+          data.forEach(function(datum) {
+            var caseSensitiveCitation;
+            caseSensitiveCitation = datum.fullCitation || datum.title;
+            resultsByFullCitation[datum.fullCitation.toLowerCase()] = datum.fullCitation || datum.title;
+          });
+
+          resultsElement.children().remove();
+          results = $.map(resultsByFullCitation, function(citation) {return "<li>" + citation + "</li>"})
+          resultsElement.append(results.join(''));
+          resultsElement.toggleClass('hidden', false);
+        }
+      })
+    }
+  }, 200);
+
+  $('#crossref-search').on('input', showCrossrefResults);
+
   $('a.delete-reference').on('click', function(e) {
     e.preventDefault();
     $(e.target).siblings('form').submit();
