660e677f917e6fba701ec204f98f9dc62195d1aa
Brian Kung <brian@callmekung.com>
2016-10-25 22:48:57 -0500

    Uses desired API for pubmed class

--- app/models/services/locators/pubmed_paper_locator.rb
+++ app/models/services/locators/pubmed_paper_locator.rb
@@ -6,34 +6,12 @@ class PubmedPaperLocator < BaseLocator
   def find_or_import_paper
     return super if super
 
-    paper = Paper.new
-
     pubmed = Pubmed.new
-    result = pubmed.get_uid_metadata(self.locator_id)
-    if (data = result['result'][self.locator_id])
-      paper = pubmed.import_data_to_paper(paper,data)
-    end
-
-    if paper.doi
-      crossref = Crossref.new
-      result = crossref.get_doi_metadata(self.locator_id)
-      if result[:error].blank?
-        data = result['message']
-        paper = crossref.import_data_to_paper(paper,data)
-      end
-    end
-
-    existing_paper = Paper.where( "title = ? or (doi = ? and doi is not null)", paper.title, paper.doi ).first
-
-    if existing_paper.present?
-      return existing_paper
-    elsif paper.save
-      return paper
+    result = pubmed.get_uid_metadata(self.locator_id)['result']
+    if (data = result[self.locator_id])
+      return Paper.create pubmed.mapper.paper_attributes
     else
-      paper.errors.delete(:title)
-      paper.errors.add(:locator_id, "is invalid; no paper found for searched Pubmed ID: #{self.locator_id}")
-      return paper
+      return nil
     end
-
   end
 end
