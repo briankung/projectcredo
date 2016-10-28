31dad2b6081fef4e470d64f5b5e90b9831064150
Brian Kung <brian@callmekung.com>
2016-10-27 23:34:16 -0500

    Adds no search result message and clears on click

--- app/assets/javascripts/crossref.js
+++ app/assets/javascripts/crossref.js
@@ -18,22 +18,30 @@ $(document).ready(function() {
       $.get('https://search.crossref.org/dois?sort=score&type=Journal+Article&rows=10&q=' + this.value).done(function(data) {
         var results;
 
-        if (data.length > 0) {
-          resultsElement.children().remove();
+        resultsElement.children().remove();
 
+        if (data.length > 0) {
           results = data.map(function(result) {
-            return '<li class="list-group-item" data-doi="' + result.doi.replace('http://dx.doi.org/', '') + '">' + result.fullCitation + "</li>"
+            return '<li class="clickable list-group-item" data-doi="' + result.doi.replace('http://dx.doi.org/', '') + '">' + result.fullCitation + "</li>"
           });
           resultsElement.append(results.join(''));
-          resultsElement.toggleClass('hidden', false);
         } else {
-          hideResultsAndClearSearchValue();
+          resultsElement.append('<li class="list-group-item">No search results found.</li>');
         };
+
+        resultsElement.toggleClass('hidden', false);
       })
     }
   }, 200);
 
   $(document).on('click', '#crossref-results .list-group-item', function(e) {
+    var clickable = $(this).hasClass('clickable');
+    if (!clickable) {
+      hideResultsAndClearSearchValue();
+      $('#crossref-search').val('');
+      $('#crossref-search').focus();
+      return;
+    };
     $('#crossref-search').val(this.innerText);
     $('#crossref-locator-id').val(this.dataset.doi);
     $('#crossref-search').toggleClass('submitted', true);
