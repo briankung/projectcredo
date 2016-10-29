ed24832f67deb2b83c05808d0512aeb9f7f429c2
Brian Kung <brian@callmekung.com>
2016-10-29 11:17:31 -0500

    Uses a different class method syntax

--- app/models/services/pubmed.rb
+++ app/models/services/pubmed.rb
@@ -1,54 +1,52 @@
 class Pubmed
   class Http
-    class << self
-      def default_parameters
-        {
-          db: 'pubmed',
-          retmode: 'json',
-          retmax: 20
-        }
-      end
+    def self.default_parameters
+      {
+        db: 'pubmed',
+        retmode: 'json',
+        retmax: 20
+      }
+    end
 
-      def base_url
-        'https://eutils.ncbi.nlm.nih.gov'
-      end
+    def self.base_url
+      'https://eutils.ncbi.nlm.nih.gov'
+    end
 
-      def esearch_url params = {}
-        generate_uri(base_url + "/entrez/eutils/esearch.fcgi", default_parameters.merge(params))
-      end
+    def self.esearch_url params = {}
+      generate_uri(base_url + "/entrez/eutils/esearch.fcgi", default_parameters.merge(params))
+    end
 
-      def esummary_url params = {}
-        generate_uri(base_url + "/entrez/eutils/esummary.fcgi", default_parameters.merge(params))
-      end
+    def self.esummary_url params = {}
+      generate_uri(base_url + "/entrez/eutils/esummary.fcgi", default_parameters.merge(params))
+    end
 
-      def efetch_url params = {}
-        generate_uri(base_url + "/entrez/eutils/efetch.fcgi", default_parameters.merge(params))
-      end
+    def self.efetch_url params = {}
+      generate_uri(base_url + "/entrez/eutils/efetch.fcgi", default_parameters.merge(params))
+    end
 
-      def esearch term:
-        parse_response(esearch_url term: term)
-      end
+    def self.esearch term:
+      parse_response(esearch_url term: term)
+    end
 
-      def esummary id:
-        parse_response(esummary_url id: id)
-      end
+    def self.esummary id:
+      parse_response(esummary_url id: id)
+    end
 
-      def efetch id: , type:
-        type = type.to_s
-        parse_response(efetch_url(id: id, retmode: type), type: type)
-      end
+    def self.efetch id: , type:
+      type = type.to_s
+      parse_response(efetch_url(id: id, retmode: type), type: type)
+    end
 
-      def generate_uri(url, parameters)
-        URI.parse(url + '?' + URI.encode_www_form(parameters))
-      end
+    def self.generate_uri(url, parameters)
+      URI.parse(url + '?' + URI.encode_www_form(parameters))
+    end
 
-      def parse_response uri, type: 'json'
-        response = Net::HTTP.get(uri)
-        if type.to_s == 'xml'
-          return Hash.from_xml(CGI.unescapeHTML response) # This breaks if unescaped content has ampersands
-        else
-          return JSON.parse(response)
-        end
+    def self.parse_response uri, type: 'json'
+      response = Net::HTTP.get(uri)
+      if type.to_s == 'xml'
+        return Hash.from_xml(CGI.unescapeHTML response) # This breaks if unescaped content has ampersands
+      else
+        return JSON.parse(response)
       end
     end
   end
