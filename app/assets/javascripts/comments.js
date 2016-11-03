f2aa9c521a28b9c6d984f628583a236e17c70ecf
Brian Kung <brian@callmekung.com>
2016-11-03 17:37:07 -0400

    Removes reply-form id

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $(document).on('click', '.toggle-reply', function(e) {
     e.preventDefault();
     commentId = $(e.target).data('parent-comment-id');
-    $("#reply-form-"+commentId).toggleClass('hidden');
+    $("#comment-"+commentId+" > form").toggleClass('hidden');
   });
 });
