ec0f3e93bb53261471665ab8bbc54c5bf88d2855
Brian Kung <brian@callmekung.com>
2016-11-04 17:05:38 -0400

    Whitespace

--- app/models/services/pubmed/resource.rb
+++ app/models/services/pubmed/resource.rb
@@ -37,17 +37,14 @@ class Pubmed
         publication:        lambda {|data| data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation Article Journal Title}},
         doi:                lambda {|data| data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation Article ELocationID}},
         pubmed_id:          lambda {|data| data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation PMID}},
-
         abstract:           lambda {|data|
           abstract = data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation Article Abstract AbstractText}
           if abstract.respond_to?(:join) then abstract.join("\n\n") else abstract end
         },
-
         published_at:       lambda {|data|
           pubdate = data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation Article Journal JournalIssue PubDate}
           Date.parse pubdate.values_at("Year", "Month", "Day").join(' ')
         },
-
         authors_attributes: lambda {|data|
           authors = data.dig *%w{PubmedArticleSet PubmedArticle MedlineCitation Article AuthorList Author}
           authors.map do |author|
