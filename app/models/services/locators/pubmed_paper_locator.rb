2367ec6cf6ece1902171e60cb57cd4ebde694668
Brian Kung <brian@callmekung.com>
2016-11-04 17:26:32 -0400

    Wires up ApiImportResponse

--- app/models/services/locators/pubmed_paper_locator.rb
+++ app/models/services/locators/pubmed_paper_locator.rb
@@ -7,10 +7,13 @@ class PubmedPaperLocator < BaseLocator
     return super if super
 
     pubmed = Pubmed.new locator_type: 'pubmed', locator_id: locator_id
-    paper_attributes = pubmed.paper_attributes
+    paper_attributes = pubmed.resource.paper_attributes
 
     if paper_attributes
-      return Paper.create paper_attributes
+      response = pubmed.resource.response
+      paper = Paper.create paper_attributes
+      ApiImportResponse.create(xml: response.body, source_uri: response.uri.to_s, paper: paper)
+      return paper
     else
       return nil
     end
