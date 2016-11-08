0c3004582c18e79e6bc0839e712c13bcea2d8cd2
Brian Kung <brian@callmekung.com>
2016-11-08 15:47:19 -0600

    Removes unnecessary type related stuff

--- app/models/services/pubmed/pubmed.rb
+++ app/models/services/pubmed/pubmed.rb
@@ -1,9 +1,17 @@
-require 'resource'
-
 class Pubmed
   attr_accessor :resource
 
   def initialize locator_type: nil, locator_id: nil
     @resource = Pubmed::Resource.new locator_type, locator_id
+  end
+
+  def self.get_uid_from_doi doi
+    response = Pubmed::Http.esearch(term: doi)
+    data = Nokogiri::XML(response.body)
+    doi_not_found = data.xpath("//PhraseNotFound").any?
+
+    return nil if doi_not_found
+
+    data.xpath('//IdList/Id').first.text
   end
 end
--- app/models/services/pubmed/resource.rb
+++ app/models/services/pubmed/resource.rb
@@ -5,17 +5,7 @@ class Pubmed
     def initialize type, id
       self.type = type.to_s
       self.id = id.to_s
-      self.get_response
-    end
-
-    def get_response
-      if type == 'doi'
-        self.response = get_details_from_doi(id)
-      elsif type == 'pubmed'
-        self.response = get_details(id)
-      else
-        self.response = nil
-      end
+      self.response = get_details(id)
     end
 
     def paper_attributes
@@ -47,25 +37,8 @@ class Pubmed
       }
     end
 
-    def get_uid_from_doi doi
-      response = Pubmed::Http.esearch(term: doi)
-      data = Nokogiri::XML(response.body)
-      doi_not_found = data.xpath("//PhraseNotFound").any?
-
-      return nil if doi_not_found
-
-      data.xpath('//IdList/Id').first.text
-    end
-
     def get_details uid
       return nil if uid.nil?
-      Pubmed::Http.efetch(id: uid)
-    end
-
-    def get_details_from_doi doi
-      return nil if doi.nil?
-
-      uid = get_uid_from_doi(doi)
       Pubmed::Http.efetch(id: uid)
     end
   end
