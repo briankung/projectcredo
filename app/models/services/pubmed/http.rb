13261ecb2b7878b8eee66467015c92f9addf9ef1
Brian Kung <brian@callmekung.com>
2016-11-11 11:35:04 -0600

    More refactoring

--- app/models/services/pubmed/http.rb
+++ app/models/services/pubmed/http.rb
@@ -1,6 +1,6 @@
 class Pubmed
   class Http
-    def self.default_parameters
+    def self.default_params
       {
         db: 'pubmed',
         retmode: 'xml',
@@ -11,23 +11,23 @@ class Pubmed
     end
 
     def self.esearch params = {}
-      get_response "/entrez/eutils/esearch.fcgi", params
+      get_response "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi", params
     end
 
     def self.esummary params = {}
-      get_response "/entrez/eutils/esummary.fcgi", params
+      get_response "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi", params
     end
 
     def self.efetch params = {}
-      get_response "/entrez/eutils/efetch.fcgi", params
+      get_response "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi", params
     end
 
     def self.idconv params = {}
-      get_response "/pmc/utils/idconv/v1.0/", params
+      get_response "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/", params
     end
 
-    def self.get_response(path, parameters)
-      uri = URI.parse("https://eutils.ncbi.nlm.nih.gov#{path}?" + URI.encode_www_form(default_parameters.merge parameters))
+    def self.get_response(path, params)
+      uri = URI.parse("#{path}?" + URI.encode_www_form(default_params.merge params))
 
       Net::HTTP.get_response(uri)
     end
