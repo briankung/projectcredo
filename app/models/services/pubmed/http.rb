0055f708ac0aa7517010f901ce713bc4d3b5480a
Brian Kung <brian@callmekung.com>
2016-11-10 18:35:24 -0600

    Refactors, adds ID service

--- app/models/services/pubmed/http.rb
+++ app/models/services/pubmed/http.rb
@@ -4,30 +4,32 @@ class Pubmed
       {
         db: 'pubmed',
         retmode: 'xml',
-        retmax: 20
+        retmax: 20,
+        tool: 'projectcredo',
+        email: 'accounts'
       }
     end
 
-    def self.esearch options = {}
-      uri = generate_uri("/entrez/eutils/esearch.fcgi", default_parameters.merge(options))
-
-      Net::HTTP.get_response(uri)
+    def self.esearch params = {}
+      get_response "/entrez/eutils/esearch.fcgi", params
     end
 
-    def self.esummary options = {}
-      uri = generate_uri("/entrez/eutils/esummary.fcgi", default_parameters.merge(options))
-
-      Net::HTTP.get_response(uri)
+    def self.esummary params = {}
+      get_response "/entrez/eutils/esummary.fcgi", params
     end
 
-    def self.efetch options = {}
-      uri = generate_uri("/entrez/eutils/efetch.fcgi", default_parameters.merge(options))
-
-      Net::HTTP.get_response(uri)
+    def self.efetch params = {}
+      get_response "/entrez/eutils/efetch.fcgi", params
     end
 
-    def self.generate_uri(url, parameters)
-      URI.parse("https://eutils.ncbi.nlm.nih.gov#{url}?" + URI.encode_www_form(parameters))
+    def self.idconv params = {}
+      get_response "/pmc/utils/idconv/v1.0/", params
+    end
+
+    def self.get_response(path, parameters)
+      uri = URI.parse("https://eutils.ncbi.nlm.nih.gov#{path}?" + URI.encode_www_form(default_parameters.merge parameters))
+
+      Net::HTTP.get_response(uri)
     end
   end
 end
