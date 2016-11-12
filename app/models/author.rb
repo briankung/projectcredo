af283e0767c60e9f47f12358360377e3bcb593d9
Brian Kung <brian@callmekung.com>
2016-11-12 16:00:58 -0600

    Fixes full-name display

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,8 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :papers
   validates :name, uniqueness: { case_sensitive: false }, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
