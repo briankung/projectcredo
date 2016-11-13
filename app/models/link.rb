59ba1235157952df1c06ddda9fd18555a50793bc
Brian Kung <brian@callmekung.com>
2016-11-13 12:38:42 -0600

    Multiline code formatting

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,10 @@
 class Link < ApplicationRecord
   belongs_to :paper
-  validates :url, format: URI::regexp(%w(http https)), presence: true, uniqueness: { scope: :paper_id,
-    message: "this link has already been added" }
+  validates :url,
+    format: URI::regexp(%w(http https)),
+    presence: true,
+    uniqueness: {
+      scope: :paper_id,
+      message: "this link has already been added"
+    }
 end
