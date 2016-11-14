5eb5528f4e0602c13942bc46cabfb735271a5c18
Brian Kung <brian@callmekung.com>
2016-11-13 18:35:37 -0600

    Validates on first/last name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,11 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :papers
-  validates :name, uniqueness: { case_sensitive: false }, presence: true
+  validates :last_name,
+    presence: true,
+    uniqueness: {
+      case_sensitive: false,
+      scope: :first_name
+    }
 
   def full_name
     "#{first_name} #{last_name}"
