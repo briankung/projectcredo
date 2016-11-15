7d942d6baa56f4b5358746012750583401767d6e
Brian Kung <brian@callmekung.com>
2016-11-14 19:35:30 -0600

    Explicitly creates ListMemberships in seeds -_-

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,6 +50,8 @@ list_names.each do |d|
   l = u.lists.build(name: d, description: d)
   l.tag_list.add(d.split)
   l.save
+  l.list_memberships.create(user: u)
+
   r1 = l.references.create(paper: p1, user: u)
   r2 = l.references.create(paper: p2, user: u)
 
