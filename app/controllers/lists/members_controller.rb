f032a52163c1c3315ac6caddc14bcb3a7561ccf8
Brian Kung <brian@callmekung.com>
2016-11-17 17:30:10 -0600

    Removes unused create action

--- app/controllers/lists/members_controller.rb
+++ app/controllers/lists/members_controller.rb
@@ -1,13 +1,6 @@
 class Lists::MembersController < ApplicationController
   before_action :ensure_current_user
 
-  def create
-    list = List.find params[:list_id]
-    member = User.find params[:id]
-    list.members.add member, role: :contributor
-    return redirect_back(fallback_location: edit_user_list_path(list.user, list))
-  end
-
   def destroy
     list = List.find params[:list_id]
     member = User.find params[:id]
