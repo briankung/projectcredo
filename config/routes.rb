d111fb6e91a7cd5b8aa7727279bd46f716e2fc38
Brian Kung <brian@callmekung.com>
2016-11-17 08:56:34 -0600

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :pins, only: [:create, :destroy]
 
-  resources :lists, except: :show do
+  resources :lists, only: [:index, :new, :create] do
     resources :references, only: [:show, :create, :destroy]
     resource :vote, controller: 'lists/votes', only: [:create, :destroy]
   end
@@ -20,9 +20,7 @@ Rails.application.routes.draw do
     resource :vote, controller: 'comments/votes', only: [:create, :destroy]
   end
 
-  get ':username' => 'users/lists#index', as: :user_profile
-  get ':username/:id' => 'users/lists#show', as: :user_list
-  get ':username/:id/edit' => 'users/lists#edit', as: :edit_user_list
-  get ':username/:id/destroy' => 'lists#destroy', as: :destroy_user_list
-  get ':username/:list_id/:id' => 'users/references#show', as: :user_reference
+  scope ':username' do
+    resources :lists, path: '/', except: [:new, :create], as: :user_lists,  controller: 'users/lists'
+  end
 end
