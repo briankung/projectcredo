18abd58c99def24586e6917a60721d1befeda4b9
Brian Kung <brian@callmekung.com>
2016-11-17 16:31:28 -0600

    Adds hack to homepage lists

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,8 +1,8 @@
 class HomepageController < ApplicationController
   def show
     if current_user
-      @pinned_lists = current_user.homepage.lists
-      @unpinned_lists = current_user.visible_lists.where.not(id: @pinned_lists.pluck(:id)).limit(9)
+      @pinned_lists = current_user.homepage.lists.uniq
+      @unpinned_lists = current_user.visible_lists.where.not(id: @pinned_lists.pluck(:id)).uniq.limit(9)
     else
       redirect_to lists_url
     end
