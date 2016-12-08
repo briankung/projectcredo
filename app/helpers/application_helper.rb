628c30debaa05fd8b9a82c03c9b9a459cd5b5b96
Brian Kung <brian@callmekung.com>
2016-12-07 21:30:00 -0600

    Automatically links URLs in text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def wrap_on_line_breaks text
-    text.to_s.split(/(?:\n\r?|\r\n?)/).map {|s| "<p>#{h s}</p>"}.join.html_safe
+    text.to_s.split(/(?:\n\r?|\r\n?)/).map {|s| autolink "<p>#{h s}</p>"}.join.html_safe
+  end
+
+  def autolink text
+    text.gsub(URI::regexp(%w(http https))) {|match| link_to match, match}
   end
 end
